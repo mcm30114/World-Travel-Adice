22b9789154ffb6a81f493326e07a8c827c6121bf
denisbosire <dbosire88@gmail.com>
2011-07-16 18:51:25 +0100

    Initial Commit

---
+++ FCOdata/FirstViewController.h
@@ -0,0 +1,16 @@
+//
+//  FirstViewController.h
+//  FCOdata
+//
+//  Created by denis bosire on 16/07/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface FirstViewController : UIViewController {
+
+}
+
+@end
---
+++ FCOdata/SecondViewController.h
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.h
+//  FCOdata
+//
+//  Created by denis bosire on 16/07/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface SecondViewController : UIViewController {
+    
+}
+
+@end
